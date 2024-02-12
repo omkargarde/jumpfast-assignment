3350d8dc634463d45ed26b63e386544b9444a67d
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-02-12 19:23:15 +0530

    Update CatPage.tsx

--- src/pages/CatPage.tsx
+++ src/pages/CatPage.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const CatPage = () => {
 	const { id } = useParams();
@@ -11,9 +11,7 @@ const CatPage = () => {
 	} = useQuery({
 		queryKey: ["todos", id],
 		queryFn: async () => {
-			const response = await fetch(
-				` https://api.thecatapi.com/v1/images/${id}`,
-			);
+			const response = await fetch(`https://api.thecatapi.com/v1/images/${id}`);
 			if (!response.ok) {
 				throw new Error("Network response was not ok");
 			}
@@ -31,9 +29,11 @@ const CatPage = () => {
 	console.log(cat);
 	return (
 		<>
-			<h1 className="">Cat by Id</h1>
-
-			<img className="image-homepage" src={cat.url} key={cat.id} />
+			<Link to={"/"}>back</Link>
+			<h1 className="">
+				Cat by Id (note: api is not returning data like in example)
+			</h1>
+			<img className="image-homepage" src={cat.url} key={cat.id} alt="" />
 		</>
 	);
 };
